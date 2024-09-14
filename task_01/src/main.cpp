9f3c193b0674dca393481345acdc8252aa166dfe
Half-Head <varinitsa.av@phystech.edu>
2024-09-14 15:12:22 +0000

    add hello world in sandbox

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,12 +1,7 @@
+#include "mediana.hpp"
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
-int mediana(int first_number, int second_number, int third_number) {
-  std ::vector<int> nums = {first_number, second_number, third_number};
-  sort(nums.begin(), nums.end());
-  return (nums[1]);
-}
 
 int main() {
 
